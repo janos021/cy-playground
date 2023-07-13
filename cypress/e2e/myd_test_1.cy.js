e6c55e4ce8b6c0019c0edef270d3e1d06a537342
Janos <Janos@wedoqa.com>
2023-07-13 11:10:12 +0200

    test and ignore

--- cypress/e2e/myd_test_1.cy.js
+++ cypress/e2e/myd_test_1.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+//describe('template spec', () => {
+//  it('passes', () => {
+//    cy.visit('https://qa2.mydirectives.com')
+//  })
+//})
+
+context ('practice cypress', () => {
+  beforeEach( () =>{
+    cy.visit ('https://qa2.mydirectives.com')
+  })
+  it ('has mydirectives header', ()=>{
+    cy.get('.normal-logo > a > img').should('exist');
+    cy.get(':nth-child(7) > a').should('contain','Returning')
   })
 })
+
