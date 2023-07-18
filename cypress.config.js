e073d9c8f1da1793545650e6f8bb72e45b025f6f
Janos <Janos@wedoqa.com>
2023-07-18 11:03:41 +0200

    import cypress-real-events

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     //watchForFileChanges; false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+     // return require("./cypress/plugins/index.js")(on, config)
     },
   },
 });
